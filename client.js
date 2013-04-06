b7fd22c4328aba9a866589366209e9c38f70ee7d
Andrew <arasmussen@katworks.com>
2013-04-05 20:18:01 -0700

    polish

--- client.js
+++ client.js
@@ -2,16 +2,16 @@ function findServerTimeOffset() {
   var socket = io.connect('http://venz.io:8080');
 
   var num_requests = 10;
-  var mean;
+  var mean = 0;
 
   socket.on('pong', function(data) {
     if (!data.hasOwnProperty('d')) {
       throw 'socket.io-time-sync: client received message with missing data';
     }
     mean += data.d / num_requests;
-    console.log('difference: ' + data.d);
-    console.log('mean: ' + mean);
   });
 
-  socket.emit('ping', {t: (new Date())});
+  for (var i = 0; i < num_requests; i++) {
+    socket.emit('ping', {t: (new Date()).valueOf()});
+  }
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function(socket) {
   socket.on('ping', function(data) {
-    var received = new Date();
+    var received = (new Date()).valueOf();
     if (!data.hasOwnProperty('t')) {
       throw 'socket.io-time-sync: server received message with missing data';
     }
