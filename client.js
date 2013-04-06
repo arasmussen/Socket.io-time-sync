79d54dc353c1b11b883937ceaee2fae2d9eb5d83
Andrew <arasmussen@katworks.com>
2013-04-05 20:11:05 -0700

    fix client

--- client.js
+++ client.js
@@ -1,17 +1,17 @@
 function findServerTimeOffset() {
-  var socket = io.connect('http://venz.io');
+  var socket = io.connect('http://venz.io:8080');
 
   var num_requests = 10;
   var mean;
 
-  socket.on('pong', function(data)) {
+  socket.on('pong', function(data) {
     if (!data.hasOwnProperty('d')) {
       throw 'socket.io-time-sync: client received message with missing data';
     }
     mean += data.d / num_requests;
     console.log('difference: ' + data.d);
     console.log('mean: ' + mean);
-  }
+  });
 
   socket.emit('ping', {t: (new Date())});
 }
